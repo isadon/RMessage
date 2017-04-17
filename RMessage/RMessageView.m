fc3482548958a1faf4f3b302ae830c5592fdbbe9
Adonis Peralta <donileo@gmail.com>
2017-04-17 11:21:13 -0400

    Add gesture recognizer for nav overlay position

--- RMessage/RMessageView.m
+++ RMessage/RMessageView.m
@@ -733,8 +733,7 @@ static NSMutableDictionary *globalDesignDictionary;
   UISwipeGestureRecognizer *gestureRecognizer =
     [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(didSwipeToDismissMessageView:)];
   [gestureRecognizer
-    setDirection:(self.messagePosition == RMessagePositionTop ? UISwipeGestureRecognizerDirectionUp :
-                                                                UISwipeGestureRecognizerDirectionDown)];
+   setDirection:(self.messagePosition == RMessagePositionBottom) ? UISwipeGestureRecognizerDirectionDown : UISwipeGestureRecognizerDirectionUp];
   [self addGestureRecognizer:gestureRecognizer];
 
   UITapGestureRecognizer *tapRecognizer =
