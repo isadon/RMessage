isadon/RMessage

commit dacab111c01807a088e4b2f389c0a6e393c8b28b

Fix positioning on iPhoneX
Use safe area layout guides feature of iOS 11 to properly position on
iPhoneX.

RMessage/RMessageView.m
@@ -28,6 +28,7 @@ static NSMutableDictionary *globalDesignDictionary;
 @property (nonatomic, weak) IBOutlet NSLayoutConstraint *titleSubtitleVerticalSpacingConstraint;
 @property (nonatomic, weak) IBOutlet NSLayoutConstraint *titleSubtitleContainerViewTrailingConstraint;
 @property (nonatomic, weak) IBOutlet NSLayoutConstraint *titleSubtitleContainerViewTopConstraint;
+@property (nonatomic, weak) IBOutlet NSLayoutConstraint *titleSubtitleContainerViewBottomConstraint;
 
 @property (nonatomic, weak) IBOutlet NSLayoutConstraint *titleLabelLeadingConstraint;
 @property (nonatomic, weak) IBOutlet NSLayoutConstraint *titleLabelTrailingConstraint;
@@ -1011,7 +1012,14 @@ static NSMutableDictionary *globalDesignDictionary;
 - (void)layoutMessageForPresentation
 {
   _titleSubtitleContainerViewTopConstraint.constant = 10.f;
-  CGFloat statusBarHeight = statusBarHeight = [self.viewController prefersStatusBarHidden] ? 0.f : ([UIApplication sharedApplication].statusBarFrame.size.height);
+
+  CGFloat statusBarHeight = self.viewController.prefersStatusBarHidden ? 0.f :
+  ([UIApplication sharedApplication].statusBarFrame.size.height);
+  CGFloat bottomOffset = 0.f;
+  if (@available(iOS 11.0, *)) {
+    statusBarHeight = self.viewController.view.safeAreaInsets.top - 10.f;
+    bottomOffset = self.viewController.view.safeAreaInsets.bottom - 10.f;
+  }
 
   UINavigationController *messageNavigationController = [self rootNavigationController];
 
@@ -1031,6 +1039,9 @@ static NSMutableDictionary *globalDesignDictionary;
     } else {
       self.titleSubtitleContainerViewTopConstraint.constant = 10.f + statusBarHeight;
     }
+  } else {
+    // Message position is RMessagePositionBottom
+    self.titleSubtitleContainerViewBottomConstraint.constant = 10.f + bottomOffset;
   }
 
   if (!self.superview) [self.viewController.view addSubview:self];

Example/DemoViewController.m
@@ -24,6 +24,12 @@
   //[[RMessageView appearance] setTitleSubtitleLabelsSizeToFit:YES];
 }
 
+// Uncomment to test hidden status bar functionality
+//- (BOOL)prefersStatusBarHidden
+//{
+//  return YES;
+//}
+
 - (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator
 {
   [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];

RMessage/Resources/RMessageView.xib
@@ -65,6 +65,7 @@
                 <outlet property="titleLabelLeadingConstraint" destination="VRO-Hp-HEg" id="sWP-AR-zAW"/>
                 <outlet property="titleLabelTrailingConstraint" destination="fhP-7h-mWV" id="V2F-nG-xMP"/>
                 <outlet property="titleSubtitleContainerView" destination="49I-AE-5sP" id="gnK-Tn-Kb9"/>
+                <outlet property="titleSubtitleContainerViewBottomConstraint" destination="4wR-Nf-WWj" id="CC6-wP-fKw"/>
                 <outlet property="titleSubtitleContainerViewTopConstraint" destination="ja8-gl-oz2" id="WOD-cL-cG9"/>
                 <outlet property="titleSubtitleContainerViewTrailingConstraint" destination="N4l-Ol-H0N" id="o4W-Zh-cRj"/>
                 <outlet property="titleSubtitleVerticalSpacingConstraint" destination="sUB-xD-36O" id="cv1-Ql-IMe"/>

commit 393dcc0e4a26d63b7dda42fbb1b69f8dc10d18c7

Fix presentation regression on iOS 11 non iPhoneX devices
Fix bug introduced in commit dacab111c that improperly offset
presentation of RMessage on non notch devices running iOS 11.

Note: Normally when presenting RMessage on non-notch devices we offset the title 10pts
from the top or bottom depending on presentation. When presenting on notch
devices there seems to be padding in the safe area so we tweak presentation
here to not offset by the normal 10pts so as to maintain some visual consistency.

RMessage/RMessageView.m
@@ -9,6 +9,7 @@
 #import "RMessageView.h"
 #import "HexColors.h"
 #import "UIViewController+PPTopMostController.h"
+#import <sys/utsname.h>
 
 static NSString *const RDesignFileName = @"RMessageDefaultDesign";
 
@@ -231,6 +232,13 @@ static NSMutableDictionary *globalDesignDictionary;
   } else {
     [superview removeConstraint:constraint];
   }
+}
+
++ (NSString *)deviceName
+{
+  struct utsname systemInfo;
+  uname(&systemInfo);
+  return [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
 }
 
 #pragma mark - Instance Methods
@@ -1023,8 +1031,23 @@ static NSMutableDictionary *globalDesignDictionary;
   ([UIApplication sharedApplication].statusBarFrame.size.height);
   CGFloat bottomOffset = 0.f;
   if (@available(iOS 11.0, *)) {
-    statusBarHeight = self.viewController.view.safeAreaInsets.top - 10.f;
-    bottomOffset = self.viewController.view.safeAreaInsets.bottom - 10.f;
+    statusBarHeight = self.viewController.view.safeAreaInsets.top;
+    bottomOffset = self.viewController.view.safeAreaInsets.bottom;
+
+    // Tweak the position to reduce what looks like visual overpadding on all devices with a notch.
+    // Unfortunately because we can't tell what device is running in the simulator this visual tweak
+    // will not show for notch devices running in the simulator unless you add the @"x86_64" or @"x86_32"
+    // value during simulator testing of notch devices.
+//    NSArray *notchDevices = @[@"iPhone10,3", @"iPhone10,6", @"x86_64"];
+    NSArray *notchDevices = @[@"iPhone10,3", @"iPhone10,6"];
+    NSString *deviceName = [[self class] deviceName];
+    for (NSString *notchDevice in notchDevices) {
+      if ([notchDevice isEqualToString:deviceName]) {
+        statusBarHeight = self.viewController.view.safeAreaInsets.top - 10.f;
+        bottomOffset = self.viewController.view.safeAreaInsets.bottom - 10.f;
+        break;
+      }
+    }
   }
 
   UINavigationController *messageNavigationController = [self rootNavigationController];