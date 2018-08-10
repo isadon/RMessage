d831bcfc87dc591757d99fbe08d176b4cbcfe740
Adonis Peralta <donileo@gmail.com>
2018-08-09 20:05:11 -0400

    Avoid use of locks and use OperationQueue
    Don't use locks in RController and instead use OperationQueue
    with a custom Operation class (RMOperation) to manage displaying
    messages.

---
+++ RMessage/RController/RMOperation.swift
@@ -0,0 +1,71 @@
+//
+//  RMOperation.swift
+//  RMessageDemo
+//
+//  Created by Adonis Peralta on 8/9/18.
+//  Copyright © 2018 None. All rights reserved.
+//
+
+import UIKit
+
+class RMOperation: Operation, RMessageDelegate {
+  override var isAsynchronous: Bool {
+    return true
+  }
+
+  private var executingState: Bool = false
+  override var isExecuting: Bool {
+    get { return executingState }
+    set {
+      willChangeValue(forKey: "isExecuting")
+      executingState = newValue
+      didChangeValue(forKey: "isExecuting")
+    }
+  }
+
+  private var finishedState: Bool = false
+  override var isFinished: Bool {
+    get { return finishedState }
+    set {
+      willChangeValue(forKey: "isFinished")
+      finishedState = newValue
+      didChangeValue(forKey: "isFinished")
+    }
+  }
+
+  private var context = 0
+
+  let message: RMessage
+
+  init(message: RMessage) {
+    self.message = message
+    super.init()
+  }
+
+  override func start() {
+    if isCancelled {
+      finish()
+      return
+    }
+
+    // Always make sure to set the message delegate to self in main as this is the only time when we can actually
+    // be the delegate (when this Operation instance has exclusive access to display the message)
+    message.delegate = self
+
+    DispatchQueue.main.async {
+      self.isExecuting = true
+      self.message.present()
+    }
+  }
+
+  // MARK: - RMessageDelegate methods
+
+  func messageDidDismiss(_ message: RMessage) {
+    finish()
+  }
+
+  private func finish() {
+    isExecuting = false
+    isFinished = true
+  }
+}
