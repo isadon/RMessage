dfe8904c3d401077fa358de4a75cf651dba7ac4c
Adonis Peralta <donileo@gmail.com>
2020-05-14 20:47:59 -0400

    Fix Tests.
    Modal presentation was changed in iOS13 causing the frames to be
    changed and modal presentation tests to fail. This commit fixes these
    issues by using the modal "base" view frame to calculate where
    RMessage should present.

--- UITests/ViewControllerTests.swift
+++ UITests/ViewControllerTests.swift
@@ -10,9 +10,10 @@ import XCTest
 
 class ViewControllerTests: XCTestCase {
   let app = XCUIApplication()
+
   lazy var navBarElement = app.navigationBars.element
   lazy var windowElement = app.windows.element.firstMatch
-  lazy var mainWindowFrame = windowElement.frame
+
   lazy var notHittablePredicate = NSPredicate(format: "hittable == FALSE")
   lazy var hittablePredicate = NSPredicate(format: "hittable == TRUE")
 
@@ -23,23 +24,32 @@ class ViewControllerTests: XCTestCase {
     super.setUp()
     continueAfterFailure = false
     XCUIApplication().launch()
+
+    app.buttons["Present Modal"].tap()
   }
 
   func showMessageFromTopByPressingButton(withName buttonName: String, timeToShow displayTimeout: TimeInterval, timeToHide dismissTimeout: TimeInterval) {
-    app.buttons["Present Modal"].tap()
-    app.buttons[buttonName].tap()
 
+    app.buttons[buttonName].firstMatch.tap()
+
+    // The base view where all the elements reside. Here we grab this to get where the position of
+    // the base view is relative to the window screen space. From this we can then calculate if the
+    // position of RMessage views is correct.
+    let baseView = app.otherElements["Modal"]
     let displayedMessage = app.otherElements["RMessage"]
+
+    let messageDisplayed = displayedMessage.waitForExistence(timeout: displayTimeout)
+
     let padding = springAnimationPadding(forHeight: Float(displayedMessage.frame.size.height))
     let expectedMsgYPosition = padding
-    let messageDisplayed = displayedMessage.waitForExistence(timeout: displayTimeout)
 
     XCTAssert(messageDisplayed, "\(buttonName) message failed to display")
 
     let expectedMessagePositionValid = validateFloatsToScale(
-      Float(displayedMessage.frame.origin.y),
+      Float(displayedMessage.frame.origin.y - baseView.frame.origin.y),
       expectedMsgYPosition, scale: kMsgYPositionScale
     )
+
     XCTAssert(expectedMessagePositionValid, "\(buttonName) message displayed in the wrong position")
 
     let exp = expectation(for: notHittablePredicate, evaluatedWith: displayedMessage, handler: nil)
@@ -47,13 +57,19 @@ class ViewControllerTests: XCTestCase {
   }
 
   func showBottomMessage(withTimeout displayTimeout: TimeInterval, timeToHide dismissTimeout: TimeInterval) {
+
     let buttonName = "Bottom"
-    app.buttons["Present Modal"].tap()
-    app.buttons[buttonName].tap()
+    app.buttons[buttonName].firstMatch.tap()
+
+    // The base view where all the elements reside. Here we grab this to get where the position of
+    // the base view is relative to the window screen space. From this we can then calculate if the
+    // position of RMessage views is correct.
+    let baseView = app.otherElements["Modal"]
 
     let displayedMessage = app.otherElements["RMessage"]
+
     let padding = springAnimationPadding(forHeight: Float(displayedMessage.frame.size.height))
-    let expectedMsgYPosition = Float(mainWindowFrame.size.height - displayedMessage.frame.size.height) - padding
+    let expectedMsgYPosition = Float((baseView.frame.origin.y + baseView.frame.size.height) - displayedMessage.frame.size.height) - padding
     let messageDisplayed = displayedMessage.waitForExistence(timeout: displayTimeout)
 
     XCTAssert(messageDisplayed, "\(buttonName) message failed to display")
@@ -64,16 +80,21 @@ class ViewControllerTests: XCTestCase {
     )
 
     XCTAssert(expectedMessagePositionValid, "\(buttonName) message displayed in the wrong position")
+
     let exp = expectation(for: notHittablePredicate, evaluatedWith: displayedMessage, handler: nil)
     wait(for: [exp], timeout: dismissTimeout)
   }
 
   func showEndlessMessage(withTimeout displayTimeout: TimeInterval) {
     let buttonName = "Endless"
-    app.buttons["Present Modal"].tap()
-    app.buttons[buttonName].tap()
+    app.buttons[buttonName].firstMatch.tap()
 
     let displayedMessage = app.otherElements["RMessage"]
+
+    // The base view where all the elements reside. Here we grab this to get where the position of
+    // the base view is relative to the window screen space. From this we can then calculate if the
+    // position of RMessage views is correct.
+    let baseView = app.otherElements["Modal"]
 
     // The spring animation padding is calculated on the message size prior to accounting for the safe area layout
     // guides so to properly determine the position of the view we must remove any safe layout are guide sizing from
@@ -85,19 +106,20 @@ class ViewControllerTests: XCTestCase {
     XCTAssert(messageDisplayed, "\(buttonName) message failed to display")
 
     let expectedMessagePositionValid = validateFloatsToScale(
-      Float(displayedMessage.frame.origin.y),
+      Float(displayedMessage.frame.origin.y - baseView.frame.origin.y),
       expectedMsgYPosition, scale: kMsgYPositionScale
     )
+
     XCTAssert(expectedMessagePositionValid, "\(buttonName) message displayed in the wrong position")
 
-    sleep(20)
+    sleep(10)
+
     XCTAssert(displayedMessage.isHittable, "\(buttonName) message no longer on screen")
   }
 
   func testButtonMessageButtonPress() {
     let buttonName = "Button"
-    app.buttons["Present Modal"].tap()
-    app.buttons[buttonName].tap()
+    app.buttons[buttonName].firstMatch.tap()
 
     let displayedMessage = app.otherElements["RMessage"]
     var messageDisplayed = displayedMessage.waitForExistence(timeout: 3.0)
@@ -119,8 +141,12 @@ class ViewControllerTests: XCTestCase {
 
   func testEndlessMessageDismiss() {
     let buttonName = "Endless"
-    app.buttons["Present Modal"].tap()
-    app.buttons[buttonName].tap()
+    app.buttons[buttonName].firstMatch.tap()
+
+    // The base view where all the elements reside. Here we grab this to get where the position of
+    // the base view is relative to the window screen space. From this we can then calculate if the
+    // position of RMessage views is correct.
+    let baseView = app.otherElements["Modal"]
 
     let displayedMessage = app.otherElements["RMessage"]
     let padding = springAnimationPadding(forHeight: Float(displayedMessage.frame.size.height - 20.0))
@@ -131,9 +157,10 @@ class ViewControllerTests: XCTestCase {
     XCTAssert(messageDisplayed, "\(buttonName) message failed to display")
 
     let expectedMessagePositionValid = validateFloatsToScale(
-      Float(displayedMessage.frame.origin.y),
+      Float(displayedMessage.frame.origin.y - baseView.frame.origin.y),
       expectedMsgYPosition, scale: kMsgYPositionScale
     )
+
     XCTAssert(expectedMessagePositionValid, "\(buttonName) message displayed in the wrong position")
 
     sleep(20)
@@ -141,10 +168,12 @@ class ViewControllerTests: XCTestCase {
     XCTAssert(displayedMessage.isHittable, "\(buttonName) message no longer on screen")
 
     displayedMessage.tap()
+
     sleep(5)
+
     XCTAssert(displayedMessage.isHittable, "\(buttonName) message no longer on screen")
 
-    app.buttons["Dismiss"].tap()
+    app.buttons["Dismiss"].firstMatch.tap()
 
     let messageDidDismiss = expectation(for: notHittablePredicate, evaluatedWith: displayedMessage, handler: nil)
     wait(for: [messageDidDismiss], timeout: 5.0)
@@ -152,8 +181,12 @@ class ViewControllerTests: XCTestCase {
 
   func testTapToDismiss() {
     let buttonName = "Error"
-    app.buttons["Present Modal"].tap()
-    app.buttons[buttonName].tap()
+    app.buttons[buttonName].firstMatch.tap()
+
+    // The base view where all the elements reside. Here we grab this to get where the position of
+    // the base view is relative to the window screen space. From this we can then calculate if the
+    // position of RMessage views is correct.
+    let baseView = app.otherElements["Modal"]
 
     let displayedMessage = app.otherElements["RMessage"]
     let padding = springAnimationPadding(forHeight: Float(displayedMessage.frame.size.height))
@@ -164,13 +197,14 @@ class ViewControllerTests: XCTestCase {
     XCTAssert(messageDisplayed, "\(buttonName) message failed to display")
 
     let expectedMessagePositionValid = validateFloatsToScale(
-      Float(displayedMessage.frame.origin.y),
+      Float(displayedMessage.frame.origin.y - baseView.frame.origin.y),
       expectedMsgYPosition, scale: kMsgYPositionScale
     )
 
     XCTAssert(expectedMessagePositionValid, "\(buttonName) message displayed in the wrong position")
 
     displayedMessage.tap()
+
     let exp = expectation(for: notHittablePredicate, evaluatedWith: displayedMessage, handler: nil)
     wait(for: [exp], timeout: 2.0)
   }
